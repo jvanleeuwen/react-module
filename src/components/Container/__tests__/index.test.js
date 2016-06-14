60aa10afce5ab2e94d8ac063b58011a490564e54
Jeroen van Leeuwen <jvanleeuwen@jouwomgeving.nl>
2016-06-14 23:17:18 +0200

    Add tests for server rendering!

--- src/components/Container/__tests__/index.test.js
+++ src/components/Container/__tests__/index.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { renderToString } from 'react-dom/server';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import Container from './../';
 
 describe('<Container />', () => {
+  it('renders on server', () => {
+    const wrapper = (
+      <Container>
+        <div className="unique" />
+      </Container>
+    );
+
+    expect(() => renderToString(wrapper)).to.not.throw(ReferenceError);
+  });
+
   it('renders children when passed in', () => {
     const wrapper = shallow(
       <Container>
--- src/components/Item/__tests__/index.test.js
+++ src/components/Item/__tests__/index.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { renderToString } from 'react-dom/server';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import Item from './../';
 
 describe('<Item />', () => {
+  it('renders on server', () => {
+    const wrapper = (
+      <Item>
+        <div className="unique" />
+      </Item>
+    );
+
+    expect(() => renderToString(wrapper)).to.not.throw(ReferenceError);
+  });
+
   it('renders children when passed in', () => {
     const wrapper = shallow(
       <Item>
